3034a315082268facc122ea6ccb8aba156025cae
Antonio3veces3 <cramirez28@ucol.mx>
2023-05-30 18:37:24 -0600

    Create testing with Jest
    Co-authored-by: BrandonVG <brandon.vadillox3@gmail.com>
    Co-authored-by: Axw3ll <axelomar.a.v@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,17 @@ module.exports = {
     es2021: true,
   },
   extends: 'airbnb-base',
-  overrides: [
-  ],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
   },
+  plugins: ['jest'],
   rules: {
     'no-console': 'off',
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
   },
 };
